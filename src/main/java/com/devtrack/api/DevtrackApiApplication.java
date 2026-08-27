744d2fa150feaf5a6c9edf55160f059e2784cb92
Konstantinos Goulas <goulaskg@csd.auth.gr>
2026-08-27 16:54:40 +0300

    feat: initialize Spring Boot project with Spring Web

---
+++ src/main/java/com/devtrack/api/DevtrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.devtrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtrack/api/DevtrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtrack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

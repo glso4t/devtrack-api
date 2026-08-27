412e8095e0f2c84af9d2af8078d550210a501ff2
Konstantinos Goulas <goulaskg@csd.auth.gr>
2026-08-27 17:25:27 +0300

    feat: add task service layer

--- src/main/java/com/devtrack/api/controller/TaskController.java
+++ src/main/java/com/devtrack/api/controller/TaskController.java
@@ -1,15 +1,23 @@
 package com.devtrack.api.controller;
 
+import com.devtrack.api.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TaskController {
-    
+
+    private final TaskService taskService;
+
+    //constructor
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping("/api/v1/tasks")
     public String getTasks() {
-        return "Tasks";
+        return taskService.getTasks();
     }
-    
+
 }
---
+++ src/main/java/com/devtrack/api/service/TaskService.java
@@ -0,0 +1,11 @@
+package com.devtrack.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    public String getTasks() {
+        return "Tasks";
+    }
+}

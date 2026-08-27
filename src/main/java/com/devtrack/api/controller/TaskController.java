9a51cb566c633496189243af1ae192be591c5952
Konstantinos Goulas <goulaskg@csd.auth.gr>
2026-08-27 20:40:52 +0300

    feat: create task endpoint + handle task not found with 404

--- src/main/java/com/devtrack/api/controller/TaskController.java
+++ src/main/java/com/devtrack/api/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.devtrack.api.controller;
 import com.devtrack.api.model.Task;
 import com.devtrack.api.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +26,14 @@ public class TaskController {
         return taskService.getTasks();
     }
 
-@PostMapping("/api/v1/tasks")
-public Task createTask(@RequestBody Task task) {
+    @GetMapping("/api/v1/tasks/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+    return taskService.getTaskById(id);
+    }
+
+    @PostMapping("/api/v1/tasks")
+    public Task createTask(@RequestBody Task task) {
     return taskService.createTask(task);
-}
+    }
 
 }
--- src/main/java/com/devtrack/api/service/TaskService.java
+++ src/main/java/com/devtrack/api/service/TaskService.java
@@ -1,7 +1,11 @@
 package com.devtrack.api.service;
 
 import com.devtrack.api.model.Task;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +13,9 @@ import java.util.List;
 @Service
 public class TaskService {
 
-    public List<Task> getTasks() {
+    private final List<Task> tasks = new ArrayList<>();
 
-        List<Task> tasks = new ArrayList<>();
-
+    public TaskService(){
         Task task1 = new Task();
         task1.setId(1L);
         task1.setTitle("Learn Spring");
@@ -27,15 +30,32 @@ public class TaskService {
         task2.setCompleted(true);
         task2.setCreatedAt(null);
 
-
         tasks.add(task1);
         tasks.add(task2);
+    }
 
+    public List<Task> getTasks() {
         return tasks;
     }
 
-    public Task createTask(Task task) {
-    return task;
+    public Task getTaskById(Long id) {
+    for (Task task : tasks) {
+        if (task.getId().equals(id)) {
+            return task;
+        }
+    }
+    //return null;
+    throw new ResponseStatusException(
+    HttpStatus.NOT_FOUND,
+    "Task not found"
+    );
 }
+
+    public Task createTask(Task task) {
+        tasks.add(task);
+        return task;
+}
+
+
 
 }
